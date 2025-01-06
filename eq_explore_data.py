9e481101c411e9619b7fc8279a1dff454e33a2b8
Efiom Ndaeyo <rylshadow@yahoo.com>
2025-01-07 02:33:13 +0800

    Extract the location data.

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -13,7 +13,7 @@ path.write_text(readable_contents)
 
 # Examine all earthquakes in the dataset.
 all_eq_dicts = all_eq_data['features']
-print(len(all_eq_dicts))
+#print(len(all_eq_dicts))
 
 # Extract the magnitude of each earthquake.
 mags = []
@@ -21,4 +21,18 @@ for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     mags.append(mag)
 
+# Extract the location data of each earthquake.
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+
+
 print(mags[:10]) 
+print(lons[:5])
+print(lats[:5])
